17a4c43cbe56ef263027fa6f6d7f39a11ccc382f
Bashar Ahmad Khan <150275763+basharahmadkhan10@users.noreply.github.com>
2026-03-23 01:23:34 +0530

    Delete My_portfolio directory

--- My_portfolio/src/components/common/ReactAtropos.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Atropos from "atropos/react";
-import "atropos/css"; // ensures 3D styles
-
-const ReactAtropos = ({ children, ...props }) => {
-  return <Atropos {...props}>{children}</Atropos>;
-};
-
-export default ReactAtropos;
